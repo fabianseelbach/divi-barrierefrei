b72f3cee149021b6bfb1da2f405d83119905e7e2
Fabian Seelbach <fabian@fsdevzone.de>
2024-12-12 16:22:21 +0000

    js errors

--- divi-barrierefrei.js
+++ divi-barrierefrei.js
@@ -6,15 +6,15 @@
     if (!document || !window) {
     return;
     }
-    var styleText = ‚::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;‘;
-    var unfocus_style = document.createElement(‚STYLE‘);
+    var styleText = '::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;';
+    var unfocus_style = document.createElement('STYLE');
     window.unfocus = function(){
-    document.getElementsByTagName(‚HEAD‘)[0].appendChild(unfocus_style);
-    document.addEventListener(‚mousedown‘, function(){
-    unfocus_style.innerHTML = styleText+‘}‘;
+    document.getElementsByTagName('HEAD')[0].appendChild(unfocus_style);
+    document.addEventListener('mousedown', function(){
+    unfocus_style.innerHTML = styleText+'}';
     });
-    document.addEventListener(‚keydown‘, function(){
-    unfocus_style.innerHTML = “;
+    document.addEventListener('keydown', function(){
+    unfocus_style.innerHTML = '';
     });
     };
     unfocus.style = function(style){
